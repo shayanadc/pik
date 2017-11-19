ea100742714e7ec04ebaf12f7d58ca8e25ed72d2
shayan <shayanadc@shayans-MacBook-Pro.local>
2017-11-19 15:54:55 +0330

    add refactor method of dividing amount between members

---
+++ app/LedgerFactory.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shayanadc
+ * Date: 11/19/17
+ * Time: 10:33 AM
+ */
+
+namespace App;
+
+
+class LedgerFactory
+{
+
+    /**
+     * LedgerFactory constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    public function divide($amount,$members,$params){
+        $sharePerPerson = $amount/ count($members);
+        $owner = $params['bill_owner'];
+        $owees = array_filter($members,function($i) use ($owner){
+            return $i != $owner;
+        });
+        $output['bill_no'] = $params['bill_no'];
+        $output['rows'] = array_map(function($owee) use ($owner, $sharePerPerson) {
+            return ['creditor' => $owner,'owee' => $owee, 'amount' => $sharePerPerson];
+        }, $owees);
+        return $output;
+    }
+}
