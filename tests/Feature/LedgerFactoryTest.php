de1f732852789ef6cc5d38892334c0573295848d
shayan <shayanadc@shayans-MacBook-Pro.local>
2017-11-21 16:36:53 +0330

    change test to one case

--- tests/Feature/LedgerFactoryTest.php
+++ tests/Feature/LedgerFactoryTest.php
@@ -27,4 +27,20 @@ class LedgerFactoryTest extends TestCase
             $outputArray);
 
     }
+    /**
+     * @test
+     */
+    public function it_calculates_members_status_in_ledger(){
+        $input = [
+            ['creditor' => 'A' , 'owee' => 'B', 'amount' => 10000],
+            ['creditor' => 'A' , 'owee' => 'B', 'amount' => 40000],
+            ['creditor' => 'A' , 'owee' => 'C', 'amount' => 20000],
+        ];
+        $ledgerFactory = new LedgerFactory();
+        $output = $ledgerFactory->calcStatus($input);
+        $this->assertEquals([
+            ['creditor' => 'A' , 'owee' => 'B', 'amount' => 50000],
+            ['creditor' => 'A' , 'owee' => 'C', 'amount' => 20000],
+        ],$output);
+    }
 }
