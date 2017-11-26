d087da3f3104d667bbf3a448af60478205133144
shayan <shayanadc@shayans-MacBook-Pro.local>
2017-11-26 23:32:06 +0330

    add bill moddel and migration

---
+++ database/migrations/2017_11_26_195110_create_bills_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('description');
+            $table->decimal('cost');
+            $table->integer('user_id');
+            $table->integer('group_id');
+            //todo: add relation with group and user
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bills');
+    }
+}
