8532c73692a7c175d63a94a5d398705789493df8
shayan <shayanadc@shayans-MacBook-Pro.local>
2017-11-26 17:05:04 +0330

    create new entry method

--- app/Group.php
+++ app/Group.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Group extends Model
 {
     protected $fillable = ['name'];
-    public static function entryNew($name){
-    return static::create(['name' => $name]);
-}
+
+    public static function entryNew($name)
+    {
+        return static::create(['name' => $name]);
+    }
 }
